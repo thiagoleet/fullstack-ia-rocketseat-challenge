eb062f608dfc8ea7616eb93692aaf8dc6a28e1f0
Thiago Leite <thiagofmleite@gmail.com>
2025-10-27 15:20:11 -0300

    Add environment configuration using Zod for validation

--- api/src/server.ts
+++ api/src/server.ts
@@ -9,6 +9,7 @@ import { fastifySwagger } from "@fastify/swagger";
 import { fastifyCors } from "@fastify/cors";
 import ScalarApiReference from "@scalar/fastify-api-reference";
 import { listWebhooks } from "./routes/list-webhooks";
+import { env } from "./env";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -43,10 +44,10 @@ app.register(listWebhooks);
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
     host: "0.0.0.0",
   })
   .then(() => {
-    console.log("🚀 HTTP server running on http://localhost:3333");
-    console.log("📚 Docs available on http://localhost:3333/docs");
+    console.log(`🚀 HTTP server running on http://localhost:${env.PORT}`);
+    console.log(`📚 Docs available on http://localhost:${env.PORT}/docs`);
   });
