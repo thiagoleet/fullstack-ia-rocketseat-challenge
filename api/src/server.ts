83b3280f6b2a08033874b9b2cc15dc6ecd0b18c8
Thiago Leite <thiagofmleite@gmail.com>
2025-10-27 15:09:43 -0300

    Refactor server setup to include Zod type provider and enhance plugin registration

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,41 @@
 import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifyCors } from "@fastify/cors";
+import ScalarApiReference from "@scalar/fastify-api-reference";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+// Compilers
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+// Plugins
+app.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  credentials: true,
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Webhook Inspector API",
+      description: "API for capture and inspecting webhook requests",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+  routePrefix: "/docs",
+});
 
 app
   .listen({
@@ -8,5 +43,6 @@ app
     host: "0.0.0.0",
   })
   .then(() => {
-    console.log("HTTP server running on http://localhost:3333");
+    console.log("🚀 HTTP server running on http://localhost:3333");
+    console.log("📚 Docs available on http://localhost:3333/docs");
   });
